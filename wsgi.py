acbefb76916b4622b651c287da442782d30823d5
Ramiro Motteta <ramox40@gmail.com>
2015-01-17 15:09:30 -0300

    Signed-off-by: Ramiro Motteta <ramox40@gmail.com>

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import os
+import sys
 
 virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
 virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
@@ -7,6 +8,10 @@ try:
     execfile(virtualenv, dict(__file__=virtualenv))
 except IOError:
     pass
+
+# add the virtualenv site-packages path to the sys.path
+sys.path.append(virtualenv + '/Lib/site-packages')
+
 #
 # IMPORTANT: Put any additional includes below this line.  If placed above this
 # line, it's possible required libraries won't be in your searchable path
